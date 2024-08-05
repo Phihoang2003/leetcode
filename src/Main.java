22d943de47e4a987e701c46bb85698acdb26c20c
Dang Hoang Phi <hoangphi1232003@gmail.com>
2024-08-05 17:35:48 +0700

    handle RemoveDuplicate

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import Medium.Merge;
-import Medium.Palindrom;
-import Medium.RemoveElement;
-import Medium.TwoSum;
+import Medium.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int [] nums = {0,1,2,2,3,0,4,2}; int val = 2;
-        List<Integer> k=new RemoveElement().removeElement(nums,val);
-        System.out.println(k);
+        int[] k={0,0,1,1,1,2,2,3,3,4};
+        int[] nums= new RemoveDuplicate().removeDuplicates(k);
+        System.out.println(Arrays.toString(Arrays.stream(nums).toArray()));
     }
 }
